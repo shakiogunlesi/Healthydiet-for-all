efcac13787bec6e50b36a975e9889e2b0b4efcb3
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-03-29 07:05:57 -0700

    Created class color
    Created class color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
 
-    <h1>Hello</h1>
+    <h1 className="text-primary">Hello</h1>
 
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+        lexend: ["Lexend", "sans-serif"],
+      },
+      colors: {
+        primary: "#DF7934",
+        primaryOrange: "#E75D14",
+        secondary: "#C5D7A1",
+        primaryGreen: "#9BC175",
+        secondaryPink: "#FEDBB3",
+      },
+      boxShadow: {
+        shadow100: "4px 3px 11.2px rgba(0, 0, 0, 0.25)",
+        shadow200: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
